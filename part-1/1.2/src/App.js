6ebafd8cfe4bb6e346750baa7cc394ff36dcf179
santmailo <iamsunil006@gmail.com>
2023-07-10 00:29:46 +0530

    part 1.a finished

--- part-1/1.2/src/App.js
+++ part-1/1.2/src/App.js
@@ -6,12 +6,17 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return(
+    <p>{props.part} {props.exercises}</p>
+)}
+
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part = {props.part1} exercises = {props.exercises1} />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
     </div>
   )
 }
@@ -31,7 +36,7 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  console.log("excercise 1.1")
+  console.log("excersice 1.2");
   return (
     <div>
     <div>
