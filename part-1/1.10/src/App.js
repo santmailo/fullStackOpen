6022fa22b4233619acc317a02796ca4e03396796
santmailo <iamsunil006@gmail.com>
2023-07-26 16:53:40 +0530

    1.11 completed

---
+++ part-1/1.10/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Button from './components/button';
+import StatisticLine from './components/statistic';
+
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad,setBad] = useState(0);
+
+  const handleGoodClick = () => setGood(good + 1);
+  const handleNeutralClick = () => setNeutral(neutral + 1);
+  const handleBadClick = () => setBad(bad + 1);
+
+
+  if(!good && !neutral && !bad) {
+    return (
+      <div className="App">
+        <h1>Give Feedback</h1>
+        <Button handleClick={handleGoodClick} text="Good" />
+        <Button handleClick={handleNeutralClick} text="Neutral" />
+        <Button handleClick={handleBadClick} text="Bad" />
+
+      </div>
+    );
+  }
+  
+  else{
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <table>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="All" value={good+neutral+bad} />
+        <StatisticLine text="Average" value={(good+neutral+bad)/3} />
+        <StatisticLine text="Positive" value={((good) / (good+neutral+bad) )* 100} />
+        </table>
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ part-1/1.10/src/components/statistic.jsx
@@ -0,0 +1,11 @@
+
+const StatisticLine = ({ text, value }) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    );
+};
+
+export default StatisticLine;
