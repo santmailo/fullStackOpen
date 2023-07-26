52d583a6819260a7aad5d67aedeed321c31b5481
santmailo <iamsunil006@gmail.com>
2023-07-26 23:34:31 +0530

    completed 2.1

---
+++ part-2/2.1/src/Components/part.jsx
@@ -0,0 +1,10 @@
+
+const Part = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+        </div>
+    )
+}
+
+export default Part;
