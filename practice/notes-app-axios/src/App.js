753629d12b5b362087d986ec40b95297c8473337
santmailo <iamsunil006@gmail.com>
2023-07-28 23:36:49 +0530

    2.12 completed

--- practice/notes-app-axios/src/App.js
+++ practice/notes-app-axios/src/App.js
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import noteService from './services/notes'
+
 import Note from './components/Note'; 
 
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+  // const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    axios.get('http://localhost:3001/notes')
+//   const hook = () => {
+//     axios.get('http://localhost:3001/notes')
+//     .then(response => {
+//       setNotes(response.data);  
+//     })
+// }
+
+//   useEffect(hook,[]);
+
+
+useEffect(() => {
+  noteService
+    .getAll()
     .then(response => {
-      setNotes(response.data);  
+      setNotes(response.data)
     })
-}
-
-  useEffect(hook,[]);
-
+    
+}, [])
 
   const addNote = (event) => {
     event.preventDefault();
@@ -26,10 +37,10 @@ function App() {
     } 
 
     
-    axios.post('http://localhost:3001/notes', noteObject)
+    noteService
+    .getAll()
     .then(response => {
-        setNotes(notes.concat(response.data));
-        setNewNote("");
+      setNotes(response.data)
     })
   }
 
@@ -39,21 +50,41 @@ function App() {
 
   // method to show all the value or filtered value
 
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true);
+  // const notesToShow = showAll
+  // ? notes
+  // : notes.filter(note => note.important === true);
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    axios.put(url, changedNote).then(response => {
+      noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      }).catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    })  }
 
 
   return (
-    <div className="App">
-      <h1>Notes</h1>
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note.content} />
-        )}
-      </ul>
+        <div className="App">
+          <h1>Notes</h1>
+          <ul>
+            {notes.map(note => 
+              <Note key={note.id} note={note} toggleImportance={() => 
+                toggleImportanceOf(note.id)
+              } />
+            )}
+          </ul>
 
-      <form onSubmit={addNote}> 
+          <form onSubmit={addNote}> 
         <input value={newNote} onChange={handleNoteChange}/>
         <button type="submit">Add</button>
       </form>
--- practice/notes-app-axios/src/components/Note.jsx
+++ practice/notes-app-axios/src/components/Note.jsx
@@ -1,8 +1,13 @@
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
 
-const Note = ({id,note}) => {
-    return (
-        <li>{note}</li>
-    )
+  return (
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  )
 }
 
 export default Note;
